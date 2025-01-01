20987a74b5f23ec56bb99596dc5a12474236afa6
Md Shadik <mdshadik232@gmail.com>
2025-01-02 00:55:52 +0530

    Cors Fix

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-// server.js
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors'); // Import the CORS package
 const stockRoutes = require('./routes/stockRoutes');
 const floorsheetRoutes = require('./routes/floorsheetRoutes');
 
 const app = express();
 const port = 10000;
+
+// Configure CORS
+app.use(cors({
+    origin: 'http://localhost:3000/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}));
 
 // Use routes
 app.use('/stocks', stockRoutes);
@@ -13,21 +20,4 @@ app.use('/floorsheet', floorsheetRoutes);
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on port ${port}`);
-  });
-  
-
-/*
-Directory Structure:
-
-controllers/
-  - stockController.js
-  - floorsheetController.js
-
-services/
-  - stockService.js
-  - floorsheetService.js
-
-routes/
-  - stockRoutes.js
-  - floorsheetRoutes.js
-*/
+});
